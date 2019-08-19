f644789e0a9efbc7a6a55491ede123142986b1b7
korkies22 <cm.sarmiento10@uniandes.edu.co>
2019-08-19 10:16:40 -0500

    Pic1 load

--- js/me.js
+++ js/me.js
@@ -4,11 +4,16 @@
 const img1 = document.querySelector(".main__img--1");
 const img2 = document.querySelector(".main__img--2");
 
-//Hides first image
-setTimeout(() => {
-  img1.classList.toggle("main__img--gone");
-}, 750);
-//Shows second image
+//Waits for image to load
+img1.onload = function() {
+  //Hides first image
+  setTimeout(() => {
+    img1.classList.toggle("main__img--gone");
+  }, 750);
+
+  //Shows second image
 setTimeout(() => {
   img2.classList.toggle("main__img--gone");
 }, 1250);
+};
+
