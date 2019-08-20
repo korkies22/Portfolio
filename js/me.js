2fdd0e132e6e2d8b4f3de0a334352a50ad3cc7c8
korkies22 <cm.sarmiento10@uniandes.edu.co>
2019-08-20 14:33:26 -0500

    debugging

--- js/me.js
+++ js/me.js
@@ -1,20 +1,19 @@
-'use strict'
+"use strict";
 
 //Select the 2 images from main
-const img1 = document.querySelector('.main__img--1')
-const img2 = document.querySelector('.main__img--2')
+const img1 = document.querySelector(".main__img--1");
+const img2 = document.querySelector(".main__img--2");
 
-window.onload = function() {
-  //Waits for image to load
-  img1.onload = function() {
-    //Hides first image
-    setTimeout(() => {
-      img1.classList.toggle('main__img--gone')
-    }, 750)
+//Waits for image to load
+img1.onload = function() {
+  console.log('hi')
+  //Hides first image
+  setTimeout(() => {
+    img1.classList.toggle("main__img--gone");
+  }, 750);
 
-    //Shows second image
-    setTimeout(() => {
-      img2.classList.toggle('main__img--gone')
-    }, 1250)
-  }
-}
+  //Shows second image
+  setTimeout(() => {
+    img2.classList.toggle("main__img--gone");
+  }, 1250);
+};
