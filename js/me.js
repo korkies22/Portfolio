3150a34e7b228138f83c2a7b2c1c190acafa8138
korkies22 <cm.sarmiento10@uniandes.edu.co>
2019-08-20 14:21:30 -0500

    Window onload to swap images

--- js/me.js
+++ js/me.js
@@ -1,18 +1,20 @@
-"use strict";
+'use strict'
 
 //Select the 2 images from main
-const img1 = document.querySelector(".main__img--1");
-const img2 = document.querySelector(".main__img--2");
+const img1 = document.querySelector('.main__img--1')
+const img2 = document.querySelector('.main__img--2')
 
-//Waits for image to load
-img1.onload = function() {
-  //Hides first image
-  setTimeout(() => {
-    img1.classList.toggle("main__img--gone");
-  }, 750);
+window.onload = function() {
+  //Waits for image to load
+  img1.onload = function() {
+    //Hides first image
+    setTimeout(() => {
+      img1.classList.toggle('main__img--gone')
+    }, 750)
 
-  //Shows second image
-  setTimeout(() => {
-    img2.classList.toggle("main__img--gone");
-  }, 1250);
-};
+    //Shows second image
+    setTimeout(() => {
+      img2.classList.toggle('main__img--gone')
+    }, 1250)
+  }
+}
